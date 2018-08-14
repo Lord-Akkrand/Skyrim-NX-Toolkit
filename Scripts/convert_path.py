1c4ed0080c4a11b34a89e219a17bf43c5b907d35
Antony Marshall <lordakkrand@gmail.com>
2018-08-14 16:47:35 +1000

    added simple progress indicator to convert_path

--- Scripts/convert_path.py
+++ Scripts/convert_path.py
@@ -18,16 +18,23 @@ def ConvertPath(mod_name, target):
 	logging.debug("This is the target: " + target)
 	logging.debug("This is the mod name " + mod_name)
 	
+	FilesToConvert = []
 	for root, subdirs, files in os.walk(target):
 		if root != target:
 			logging.debug("Walking folder " + root)
 			for filename in files:
 				if filename.lower().endswith(".dds"):
 					file_path = os.path.join(root, filename)
-					logging.debug("ModConverting " + file_path)
-					convert_dds.ConvertDDS(target, file_path)
-
-
+					FilesToConvert.append(file_path)
+	logging.info("Found {} dds files to convert".format(len(FilesToConvert)))
+	
+	for i in range(len(FilesToConvert)):
+		file_path = FilesToConvert[i]
+		convert_dds.ConvertDDS(target, file_path)
+		sys.stdout.write("Converted {}/{} \r".format(i+1, len(FilesToConvert)))
+		sys.stdout.flush()
+	sys.stdout.write("\n")
+	
 if __name__ == '__main__':
 	mod_name = sys.argv[1]
 	target = sys.argv[2]

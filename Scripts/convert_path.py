16c62a4f8883e069187ee1348e3f9bf9cca4255c
Antony Marshall <lordakkrand@gmail.com>
2018-09-07 10:47:07 +1000

    added beta sound conversion

--- Scripts/convert_path.py
+++ Scripts/convert_path.py
@@ -5,10 +5,10 @@ import os.path
 import shutil
 import subprocess
 import util
-import logging
 import convert_dds
 import convert_hkx
 import convert_txt
+import convert_sound
 
 import bitflag
 
@@ -25,6 +25,7 @@ def ConvertPath(mod_name, target):
 	ConvertListDDS = []
 	ConvertListHKX = []
 	ConvertListTXT = []
+	ConvertListSound = []
 	for root, subdirs, files in os.walk(target):
 		if root != target:
 			util.LogDebug("Walking folder " + root)
@@ -38,26 +39,32 @@ def ConvertPath(mod_name, target):
 				elif filename.lower().startswith("translate_") and filename.lower().endswith(".txt"):
 					file_path = os.path.join(root, filename)
 					ConvertListTXT.append(file_path)
+				elif filename.lower().endswith(".xmw") or filename.lower().endswith(".fuz") or filename.lower().endswith(".wav"):
+					file_path = os.path.join(root, filename)
+					ConvertListSound.append(file_path)
 					
 	util.LogInfo("Found {} dds files to convert".format(len(ConvertListDDS)))
 	if has_havoc: util.LogInfo("Found {} hkx files to convert".format(len(ConvertListHKX)))
 	util.LogInfo("Found {} txt files to convert".format(len(ConvertListTXT)))
+	util.LogInfo("Found {} sound files to convert".format(len(ConvertListSound)))
 	
 	def LogProgress(convertList, convertFn, name):
-		failedCount = 0
-		for i in range(len(convertList)):
-			file_path = convertList[i]
-			success = convertFn(target, file_path)
-			if not success:
-				failedCount += 1
-			sys.stdout.write("Converted {}/{} {} ({}) failed. \r".format(i+1, len(convertList), name, failedCount))
-			sys.stdout.flush()
-		sys.stdout.write("\n")
+		if len(convertList) > 0:
+			failedCount = 0
+			for i in range(len(convertList)):
+				file_path = convertList[i]
+				success = convertFn(target, file_path)
+				if not success:
+					failedCount += 1
+				sys.stdout.write("Converted {}/{} {} ({}) failed. \r".format(i+1, len(convertList), name, failedCount))
+				sys.stdout.flush()
+			sys.stdout.write("\n")
 	
 	LogProgress(ConvertListDDS, convert_dds.ConvertDDS, "DDS")
 	if has_havoc:
 		LogProgress(ConvertListHKX, convert_hkx.ConvertHKX, "HKX")
 	LogProgress(ConvertListTXT, convert_txt.ConvertTXT, "TXT")
+	LogProgress(ConvertListSound, convert_sound.ConvertSound, "Sounds")
 	
 if __name__ == '__main__':
 	mod_name = sys.argv[1]
---
+++ Scripts/convert_sound.py
@@ -0,0 +1,36 @@
+#! python3
+
+import os
+import util
+
+def GetTool():
+	utilities_path = util.GetUtilitiesPath()
+	soundconverter = os.path.join(utilities_path, "Sound", "Skyim NX Audio-Voice Dialog Converter.exe")
+	return soundconverter
+
+	
+def ConvertSound_Internal(filepath):
+	
+	# default, if in doubt, use mcadpcm
+	argument = "-mcadpcm"
+	filename = os.path.basename(filepath)
+	util.LogDebug("Convert Sound <{}>".format(filename))
+	if filename.endswith(".fuz"):
+		argument = "-fuz"
+	elif "_lp." in filename or "_lpm." in filename:
+		argument = "-mcwav"
+		
+	soundconverter = GetTool()
+	commandLine = [soundconverter, argument, filepath]
+	(convOutput, convErrors) = util.RunCommandLine(commandLine)
+	
+	if "Unknown Error" in convOutput or "Not Implemented" in convOutput:
+		return False
+	return True
+	
+def ConvertSound(target, filepath):
+	return ConvertSound_Internal(filepath)
+	
+if __name__ == '__main__':
+	filepath = sys.argv[1]
+	ConvertSound(filepath)

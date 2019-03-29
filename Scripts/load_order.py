0f0b942046a7dc1310746ebf50137e51b942ab2b
Paulo Monteiro <pauloesquilo@gmail.com>
2019-03-29 07:11:05 -0700

    removed the small typo on Voices RegEx

--- Scripts/load_order.py
+++ Scripts/load_order.py
@@ -182,7 +182,7 @@ def LoadOrder(origin, target, loadOrderName):
 					InsertTestFile(file, filename)
 				elif re.match(".*Textures[0-9]?.bsa$", file):
 					InsertLanguageBSA(file, filename)
-				elif re.match(".*Voices.[0-9]?.bsa$", file):
+				elif re.match(".*Voices[0-9]?.bsa$", file):
 				elif file.endswith("Animations.bsa"):
 					InsertInMemoryBSA(file, filename)
 				elif file.endswith(".bsa"):

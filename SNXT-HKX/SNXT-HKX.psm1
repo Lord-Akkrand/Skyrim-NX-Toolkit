b9fc013b9ee7f3a6a8b0442e4f7f9aa2e78b0ccb
Antony Marshall <lordakkrand@gmail.com>
2019-07-30 11:28:00 +1000

    hkx64 working

--- SNXT-HKX/SNXT-HKX.psm1
+++ SNXT-HKX/SNXT-HKX.psm1
@@ -94,6 +94,55 @@ function Convert-HKX32([string] $fullpath, [hashtable] $info)
     }
 }
 
+function Find-FirstMatch([byte[]] $source, [byte []]$target)
+{
+    $sourceLen = $source.Length
+    $targetLen = $target.Length
+    for ($i=0; $i -lt $sourceLen; $i++)
+    {
+        if ($source[$i] -eq $target[0])
+        {
+            $matchIndex = $i
+            for ($j=1; $j -lt $targetLen; $j++)
+            {
+                $sourceIndex = $i+$j
+                if ($sourceIndex -ge $sourceLen)
+                {
+                    #Write-Host ('Reached sourceIndex {0} at sourceLen {1}' -f $sourceIndex, $sourceLen)
+                    $matchIndex = -1
+                    break
+                }
+                if ($source[$sourceIndex] -ne $target[$j])
+                {
+                    #Write-Host ('source[{0}]{1} ne target[{2}]{3}' -f $sourceIndex, $source[$sourceIndex], $j, $target[$j])
+                    $matchIndex = -1
+                    break
+                }
+            }
+            if ($matchIndex -ge 0)
+            {
+                return $matchIndex
+            }
+        }
+    }
+    return -1
+}
+
+function Slice-Array([byte []] $data, [int]$startSlice, [int]$endSlice)
+{
+    if ($startSlice -lt 0)
+    {
+        $startSlice = $data.Length + $startSlice + 1
+    }
+    if ($endSlice -lt 0)
+    {
+        $endSlice = $data.Length + $endSlice + 1
+    }
+    #python slicing doesn't include the last entry in a slice
+    $endSlice -= 1
+    return $data[$startSlice..$endSlice]
+}
+
 function Convert-HKX64([string] $fullpath, [hashtable] $info)
 {
     Begin
@@ -103,12 +152,64 @@ function Convert-HKX64([string] $fullpath, [hashtable] $info)
         Trace-Debug ('Convert-HKX64 RelativeFilename="{0}"' -f $relativeFilename) $LogTreeFilename 1
     }
     
+    
     Process
     {
         $retValue = @{}
         
-        Trace-Verbose ('Manually Fix="{0}"' -f $nfo.Type) $LogTreeFilename
+        Trace-Verbose ('Manually Fix="{0}"' -f $info.Type) $LogTreeFilename
+
+        #$payload = [array](Get-Content -Path $fullpath -Encoding Byte)
+
+        [byte[]] $payload = [System.Io.File]::ReadAllBytes( $fullpath )
+        $hkaSkeleton = [System.Text.Encoding]::ASCII.GetBytes('hkaSkeleton')
         
+        #Trace-Verbose ('Payload Bytes="{0}"' -f [string]$payload) $LogTreeFilename
+
+        Trace-Verbose ('Test hkaSkeleton="{0}"' -f [string]$hkaSkeleton) $LogTreeFilename
+        $findHkaSkeleton = Find-FirstMatch $payload $hkaSkeleton
+        if($findHkaSkeleton -ge 0)
+        {
+            Trace-Debug('Warning hkaSkeleton="{0}"' -f $findHkaSkeleton) $LogTreeFilename
+            $retValue.Sucess = $False
+            return $retValue
+        }
+
+        $hkbBehavior = [System.Text.Encoding]::ASCII.GetBytes('hkbBehavior')
+        Trace-Verbose ('Test hkbBehavior="{0}"' -f [string]$hkbBehavior) $LogTreeFilename
+        $findHkaBehavior = Find-FirstMatch $payload $hkbBehavior
+        if($findHkaBehavior -ge 0)
+        {
+            Trace-Debug('Warning hkbBehavior="{0}"' -f $findHkaBehavior) $LogTreeFilename
+            $retValue.Sucess = $False
+            return $retValue
+        }
+
+        [byte []] $header = (,0x00 * 15) + (,0x80) + (,0x00 * 48)
+        Trace-Verbose ('Find header="{0}"' -f [string]$header) $LogTreeFilename
+
+        $headerStartIndex = Find-FirstMatch $payload $header
+        Trace-Verbose ('HeaderStartIndex Value="{0}"' -f $headerStartIndex) $LogTreeFilename
+        if ($headerStartIndex -gt 0)
+        {
+            $hkbProjectData = [System.Text.Encoding]::ASCII.GetBytes('hkbProjectData')
+            Trace-Verbose ('Test hkbProjectData="{0}"' -f [string]$hkbProjectData) $LogTreeFilename
+            $offset = 76
+            $findProjectData = Find-FirstMatch $payload $hkbProjectData
+            if ($findProjectData -ge 0)
+            {
+                $offset = 72
+            }
+            [byte []] $newPayload = @()
+            $newPayload += Slice-Array $payload 0 18
+            $newPayload += 0x01
+            $newPayload += Slice-Array $payload 19 ($headerStartIndex+16)
+            $newPayload += Slice-Array $payload ($headerStartIndex+20) ($headerStartIndex+$offset)
+            $newPayload += Slice-Array $payload ($headerStartIndex) ($headerStartIndex+4)
+            $newPayload += Slice-Array $payload ($headerStartIndex+$offset) -1
+
+            [System.Io.File]::WriteAllBytes( $fullpath, $newPayload )
+        }
         $retValue.Success = $True
         return $retValue
     }

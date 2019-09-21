fb3cbcb3281c9a38b3f3d1629128e0f0e4ae48af
ThyWoof <pauloesquilo@gmail.com>
2019-09-21 11:10:49 -0700

    tested and ready for final method

--- Scripts/chain_mod.py
+++ Scripts/chain_mod.py
@@ -17,28 +17,34 @@ def get_mods(load_order_filename):
 	return mods_list
 
 def get_mods_master_payload(mods_list):
+	SSE_MODS = ['Skyrim.esm', 'Update.esm', 'Dawnguard.esm', 'HearthFires.esm', 'Dragonborn.esm']
 	mods_master_payload = {}
 	master_payload = b''
 	for mod in mods_list:
-		_len = len(mod) + 1
-		master_payload = master_payload + \
-			b'MAST' + \
-			_len.to_bytes(0x02, byteorder = 'little') + \
-			mod.encode('latin-1') + \
-			b'\x00' + \
-			b'DATA' + \
-			b'\x08\x00' + \
-		    b'\x00\x00\x00\x00' + \
-			b'\x00\x00\x00\x00'
-		mods_master_payload[mod] = master_payload
+		payload = master_payload + \
+			b'MAST' + (len(mod) + 1).to_bytes(0x02, byteorder = 'little') + mod.encode('latin-1') + b'\x00' + \
+			b'DATA' + b'\x08\x00' + b'\x00\x00\x00\x00' + b'\x00\x00\x00\x00'
+		if not mod in SSE_MODS:
+			mods_master_payload[mod] = master_payload
+		master_payload = payload
 	return mods_master_payload
 
+def chain_mods(data_folder, mods_master_payload):
+	util.LogInfo("found {} mods to chain".format(len(mods_master_payload)))
+	for mod in mods_master_payload:
+		mod_filename = os.path.join(data_folder, mod)
+		if not os.path.exists(mod_filename):
+			util.LogInfo("skipping '{}' as file not found".format(mod_filename))
+		else:
+			chain_single_mod(mod_filename, mods_master_payload[mod])
+
+def chain_single_mod(mod_filename, master_payload):
+	util.LogInfo("chaining '{}'".format(mod_filename))
+
 def ChainMod(data_folder, load_order_filename):
-	print(data_folder)
 	mods_list = get_mods(load_order_filename)
-	print(mods_list)
 	mods_master_payload = get_mods_master_payload(mods_list)
-	print(mods_master_payload)
+	chain_mods(data_folder, mods_master_payload)
 
 def ChainMod_External(data_folder, load_order_filename):
 	util.InitialiseLog(os.path.join(data_folder, "chain_mod.log"))
